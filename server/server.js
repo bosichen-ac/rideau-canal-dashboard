489f2c91d158fde446f5dc020447eef23378978c
Bosi Chen <chenbosi.bosi@gmail.com>
2026-04-16 18:11:36 -0400

    connect to cosmos db

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { CosmosClient } = require("@azure/cosmos");
 
-app.use(express.json());
+const app = express();
+app.use(cors());
+
+const client = new CosmosClient({
+  endpoint: process.env.COSMOS_ENDPOINT,
+  key: process.env.COSMOS_KEY
+})
+
+const database = client.database(process.env.COSMOS_DATABASE);
+const container = database.container(process.env.COSMOS_CONTAINER);
+
 
 app.get("/", (req, res) => {
-  res.send("Success");
+  res.send("API...");
+});
+
+app.get("/api/analytics", async (req, res) => {
+  try {
+    const query = {
+      query: "SELECT * FROM c ORDER BY c.windowEnd DESC"
+    };
+
+    const { resources } = await container.items.query(query).fetchAll();
+
+    res.json(resources);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching data");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
